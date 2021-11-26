0c9c5c2b6222da5125e4907bdbee81ff9a8cfd46
Lizeth <julia.sucasacai@gmail.com>
2021-11-25 21:38:32 -0600

    increment

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+ 
+    constructor(props) {
+      super(props);
+      this.state = {
+        clicks: 0
+      };
+    }
+
+    IncrementItem = () => {
+      this.setState({ clicks: this.state.clicks + 1 });
+      let clic = this.state.clicks + 1;
+      console.log(clic );
+    }
+
   render() {
+
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.clicks}</span>
+        <button onClick={this.IncrementItem} id="inc">Incrementa</button>
       </div>
     );
   }
